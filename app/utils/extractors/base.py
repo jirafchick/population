a4ff649bb0c3cb72c9718554732f96d925b8e025
OleksandrPrudyvus <pryduvyso20@gmail.com>
2024-08-08 11:22:33 +0300

    Updated base extractor hints

--- app/utils/extractors/base.py
+++ app/utils/extractors/base.py
@@ -6,7 +6,7 @@ import pandas as pd
 class AbstractBaseExtractor(ABC):
 	"""Abstract base class for data extractors."""
 
-	def __init__(self, content: str):
+	def __init__(self, content):
 		self._data_frame = pd.read_html(str(content))[0]
 
 	@abstractmethod
